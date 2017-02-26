4bdf055f6c17537e5b9c59d4f865752cd36970b3
ghostFaceKillah <garmulewicz@gmail.com>
2017-02-26 23:05:17 +0100

    Updated the output image to have text information

--- process_video.py
+++ process_video.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import argparse
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn
+import ipdb
 from moviepy.editor import VideoFileClip
 
 from color_grad import combined_thresholding
@@ -24,14 +26,25 @@ class Processor(object):
         self.l_curv_hist = []
         self.r_curv_hist = []
         self.center_dist_hist = []
+        self.l_curv = 0.
+        self.r_curv = 0.
+        self.center_dist = None
 
     def update_fit(self, new_left_fit, new_right_fit):
         a = self.alpha
         self.l_fit = a * self.l_fit + (1 - a) * new_left_fit
         self.r_fit = a * self.r_fit + (1 - a) * new_right_fit
 
+    def update_curvature_stats(self):
         # Update the curvature and center-of-line stats
         stats = calculate_curvature_and_center(self.l_fit, self.r_fit)
+        l_curv, r_curv, center_dist = stats
+
+        a = 0.9
+        self.l_curv = a * self.l_curv + (1 - a) * l_curv
+        self.r_curv = a * self.r_curv + (1 - a) * r_curv
+        self.center_dist = center_dist
+
         self.l_curv_hist.append(stats[0])
         self.r_curv_hist.append(stats[1])
         self.center_dist_hist.append(stats[2])
@@ -53,6 +66,15 @@ class Processor(object):
         plt.savefig('out/dist_from_center.jpg')
         plt.close()
 
+    def draw_curvature_center_info(self, img):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        txt = "Left rad. of curv = {:.2f} m, right rad. of curv = {:.2f} m, "\
+              "distance from center = {:.2f} m"
+        f = lambda x: np.round(x / 10.) * 10.
+        txt = txt.format(f(self.l_curv), f(self.r_curv), self.center_dist)
+        out = cv2.putText(img, txt, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, 
+                          (0,0,0), 1, cv2.LINE_AA)
+        return out
 
     def process_image(self, img):
         undistorted = self.undistort(img)
@@ -67,7 +89,10 @@ class Processor(object):
             l_fit, r_fit = find_next_lane(binary_warped, self.l_fit, self.r_fit)
             self.update_fit(l_fit, r_fit)
 
+        self.update_curvature_stats()
+
         out = draw_fit_on_original(undistorted, self.l_fit, self.r_fit)
+        out = self.draw_curvature_center_info(out)
         return out
 
 
@@ -82,7 +107,6 @@ def main():
     output_video_file = args.out_video
 
     clip = VideoFileClip(video_file)
-    # clip = clip.subclip(t_start=0.0, t_end=2.0)
 
     processor = Processor()
 
